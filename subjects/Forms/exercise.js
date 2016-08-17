cf9b8aebcb5f570124736bc77399fd6659f781e0
Lance Cranford <lcranf@gmail.com>
2016-08-16 21:36:09 -0400

    completed Forms exercise

--- subjects/Forms/exercise.js
+++ subjects/Forms/exercise.js
@@ -21,33 +21,85 @@ import { render } from 'react-dom'
 import serializeForm from 'form-serialize'
 
 const CheckoutForm = React.createClass({
+  getInitialState() {
+    return {
+      billingName: '',
+      billingState: '',
+      shippingName: '',
+      shippingState: '',
+      sameAsBilling: false,
+      serializedForm: {}
+    }
+  },
+
+  updateSameAsBilling(event) {
+    this.setState({
+      sameAsBilling: event.target.checked
+    })
+  },
+
+  printFormValues(event) {
+    event.preventDefault()
+    const serializedData = serializeForm(event.target, { hash: true })
+    this.setState({ serializedForm: serializedData })
+  },
+
   render() {
+    const {
+      billingName,
+      billingState,
+      shippingName,
+      shippingState,
+      sameAsBilling
+    } = this.state
+
     return (
       <div>
         <h1>Checkout</h1>
-        <form>
+        <pre>{ JSON.stringify(this.state, null, 2) }</pre>
+        <form onSubmit={this.printFormValues}>
           <fieldset>
             <legend>Billing Address</legend>
             <p>
-              <label>Billing Name: <input type="text"/></label>
+              <label>Billing Name: <input
+                                        type="text"
+                                        name="billingName"
+                                        defaultValue={billingName}
+                                        onChange={event => this.setState({ billingName: event.target.value })}/></label>
             </p>
             <p>
-              <label>Billing State: <input type="text" size="2"/></label>
+              <label>Billing State: <input type="text"
+                                           name="billingState"
+                                           defaultValue={billingState}
+                                           onChange={event => this.setState({ billingState: event.target.value })}
+                                           size="2"/></label>
             </p>
           </fieldset>
 
           <br/>
 
           <fieldset>
-            <label><input type="checkbox"/> Same as billing</label>
+            <label><input type="checkbox" onChange={this.updateSameAsBilling}/> Same as billing</label>
             <legend>Shipping Address</legend>
             <p>
-              <label>Shipping Name: <input type="text"/></label>
+              <label>Shipping Name: <input
+                                        type="text"
+                                        name="shippingName"
+                                        value={sameAsBilling ? billingName : shippingName}
+                                        disabled={sameAsBilling}
+                                        onChange={event => this.setState({ shippingName: event.target.value })}/></label>
             </p>
             <p>
-              <label>Shipping State: <input type="text" size="2"/></label>
+              <label>Shipping State: <input
+                                        type="text"
+                                        name="shippingState"
+                                        value={sameAsBilling ? billingState : shippingState}
+                                        disabled={sameAsBilling}
+                                        onChange={event => this.setState({ shippingState: event.target.value })}
+                                        size="2"/></label>
             </p>
           </fieldset>
+          <button type="submit">Save</button>
         </form>
       </div>
     )
